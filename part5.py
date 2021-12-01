08622a837e9376398deccfafdf5739b139672b7d
Ila Chander <93930100+ila-chander@users.noreply.github.com>
2021-12-01 02:39:31 +0000

    --

--- part5.py
+++ part5.py
@@ -23,3 +23,32 @@ not a month
 '''
 
 #start writing your code below
+
+month = str(input("enter a month: "))
+
+if month == "january" or month == "march" or month == "may" or month == "july" or month == "august" or month == "october" or month =="december":
+  print(31)
+elif month == "april" or month == "june" or month =="september" or month == "november":
+  print(30)
+elif month == "february":
+  print("28 or 29")
+else:
+  print("not a month")
+
+
+'''
+i started this to test my memory of lists
+
+months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+
+days = [ 31, "28 or 29", 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
+
+print(list(months))
+print(list(days))
+
+for i in range(12):
+  if month(i) == month:
+    print(days(i))
+  else:
+    print("not a month.")
+'''
